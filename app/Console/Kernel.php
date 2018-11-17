586fcacb158b6e9ceab6bdd8d780c39204c7a161
Ramdan <mramdanf@gmail.com>
2018-11-17 19:53:32 +0700

    change scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('CacheDataHotspotSipongi:cachehotspot')
-                    ->dailyAt('16:00');
+                     ->dailyAt('13:00');
+                     //->everyMinute();
     }
 
     /**
