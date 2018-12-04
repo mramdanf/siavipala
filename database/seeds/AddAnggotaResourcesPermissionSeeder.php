7be9d3af8a14c930bf15a954c04b475d856d0cf3
Ramdan <ramdan.firdaus@metrasat.co.id>
2018-12-05 05:50:47 +0700

    permission for resources anggoat

---
+++ database/seeds/AddAnggotaResourcesPermissionSeeder.php
@@ -0,0 +1,86 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Role;
+use App\Models\Permission;
+
+class AddAnggotaResourcesPermissionSeeder extends Seeder
+{
+    protected $roles = [];
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->roles = [
+            
+            [
+                'name' => 'administrator',
+                'pengguna' => [
+                    'ramdan'
+                ],
+                'permissions' => [
+                    'anggota-create',
+                    'anggota-update',
+                    'anggota-delete'
+                ]
+            ],
+
+        ];
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createPermissions();
+
+        $this->assignPermissionRole();
+    }
+
+    private function createPermissions()
+    {
+        foreach($this->roles as $role)
+        {
+            foreach($role['permissions'] as $permission)
+            {
+                $newPermission = new Permission();
+                $newPermission->name = $permission;
+                $newPermission->display_name = ucwords(str_replace('-', ' ', $permission));
+                $newPermission->save();
+            }
+        }
+    }
+
+    private function assignPermissionRole()
+    {
+        foreach($this->roles as $key_roles => $role)
+        {
+            $roleInDb = Role::where('name', 'ilike', '%'.$role['name'].'%')->first();
+            if ($roleInDb != NULL)
+            {
+                foreach($role['permissions'] as $key_permission => $permission)
+                {
+                    $permissionInDb = Permission::where('name', 'ilike', '%'.$permission.'%')->first();
+                    if ($permissionInDb)
+                    {
+                        $roleInDb->attachPermission($permissionInDb);
+                        if ($roleInDb->hasPermission($permissionInDb->name))
+                        {
+                            $this->roles[$key_roles]['permissions'][$key_permission] = [];
+                            $this->roles[$key_roles]['permissions'][$key_permission]['name']   = $permissionInDb['name'];
+                            $this->roles[$key_roles]['permissions'][$key_permission]['status'] = 'OK';
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
