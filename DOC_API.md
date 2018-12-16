db6b9abcf27bb3f6f764697f6964a48a7c1cc1f5
Ramdan <ramdan.firdaus@metrasat.co.id>
2018-12-16 13:25:00 +0700

    filter keterangan lokasi

--- DOC_API.md
+++ DOC_API.md
@@ -1318,11 +1318,11 @@
 
 * **Headers**
 
-  * `Authroization: Bearer {{ Token }}`
+  `none`
 
 * **Data Param**
 
-  `none`
+  * `key: search key (opsional)`
 
 * **Success Response**
 
--- app/Http/Controllers/KeteranganLokasiController.php
+++ app/Http/Controllers/KeteranganLokasiController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\KeteranganLokasi;
+
 class KeteranganLokasiController extends Controller
 {
-    public function list()
+    public function list(Request $r)
     {
+        if ($r->has('key'))
+        {
+            $data = KeteranganLokasi::where('nama', 'ilike', '%'.$r->input('key').'%')->get();
+            return response([
+                'data' => $data
+            ]);
+        }
+
         return response([
-            'data' => 'App\Models\KeteranganLokasi'::all()
+            'data' => KeteranganLokasi::all()
         ]);
     }
 }
