219dce57aed2189946c6fee514f781180fb77c72
Ramdan <ramdan.firdaus@metrasat.co.id>
2018-12-16 11:56:41 +0700

    filter inventori patroli

--- DOC_API.md
+++ DOC_API.md
@@ -974,11 +974,11 @@
 
 * **Headers**
 
-  * `Authroization: Bearer {{ Token }}`
+  * `none`
 
 * **Data Param**
 
-  `none`
+  * `key: search key (opsional)`
 
 * **Success Response**
 
--- app/Http/Controllers/InventoriController.php
+++ app/Http/Controllers/InventoriController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Inventori;
+
 class InventoriController extends Controller
 {
-    public function list()
+    public function list(Request $r)
     {
+        if ($r->has('key'))
+        {
+            $data = Inventori::where('nama', 'ilike', '%'.$r->input('key').'%')->get();
+            return response([
+                'data' => $data
+            ]);
+        }
+
         return response([
-            'data' => 'App\Models\Inventori'::all()
+            'data' => Inventori::all()
         ]);
     }
 }
