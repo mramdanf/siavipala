c5cc724498e592ae2511961bc7de351e96b396af
Ramdan <ramdan.firdaus@metrasat.co.id>
2018-12-16 12:02:46 +0700

    filter jenis tanah

--- app/Http/Controllers/TanahController.php
+++ app/Http/Controllers/TanahController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Tanah;
+
 class TanahController extends Controller
 {
-    public function list()
+    public function list(Request $r)
     {
+        if ($r->has('key'))
+        {
+            $data = Tanah::where('nama', 'ilike', '%'.$r->input('key').'%')->get();
+            return response([
+                'data' => $data
+            ]);
+        }
+
         return response([
-            'data' => 'App\Models\Tanah'::all()
+            'data' => Tanah::all()
         ]);
     }
 }
